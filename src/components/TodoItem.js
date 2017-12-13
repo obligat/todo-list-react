d3760bbb400ca948c681d2e2890e4b5f6adf5953
dujinqiao <duzi1996@qq.com>
2017-12-13 20:05:27 +0800

    Add Item is created time

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 export default function (props) {
 
+    let formatTime = '';
+    if(props.item.id){
+        formatTime = new Date(props.item.id).toISOString().match(/(\d{4}-\d{2}-\d{2})/)[1];
+    }
 
     return (<li className="list-group-item">
         <input type="checkbox"
                onChange={(e) => props.handleChange(props.item.id, e)}
                checked={props.item.isDone}/>
         <span style={{textDecoration: props.item.isDone ? "line-through" : ""}}>
-            {props.item.text}
+            {props.item.text} &nbsp;&nbsp;
         </span>
+        <span>{formatTime}</span>
         <button type="button"
                 className="close pull-right"
                 aria-hidden="true"
